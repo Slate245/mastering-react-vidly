7014ddd52b1fa139aef19104329981951544385a
Slate245 <slate245@gmail.com>
2019-06-08 21:04:43 +0500

    First exsercise (no comments)

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,60 +6,10 @@ class Movies extends Component {
     movies: getMovies()
   };
 
-  /* handleDelete = movieId => {
-    let indexToDelete = this.state.movies.findIndex(
-      movie => movie._id === movieId
-    );
-    this.state.movies.splice(indexToDelete, 1);
-    this.setState({ movies: this.state.movies });
-  }; */
-
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
   };
-
-  /* renderMessage() {
-    let numberOfMovies = this.state.movies.length;
-    if (numberOfMovies > 0)
-      return `Showing ${numberOfMovies} movies in the database.`;
-    else return "There are no movies in the database.";
-  }
-
-  renderTable() {
-    if (this.state.movies.length > 0)
-      return (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-              <th scope="col" />
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.map(movie => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      );
-  } */
 
   render() {
     const { length: moviesCount } = this.state.movies;
