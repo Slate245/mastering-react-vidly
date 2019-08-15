02914839d006c97bc140ba3dc15464dc8181e17c
Slate245 <slate245@gmail.com>
2019-08-15 16:02:28 +0500

    Connect MovieForm to the Backend (My solution)

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -10,3 +10,29 @@ export function getMovies() {
 export function deleteMovie(movieId) {
   return http.delete(`${apiEndpoint}/${movieId}`);
 }
+
+export function getMovie(movieId) {
+  try {
+    return http.get(`${apiEndpoint}/${movieId}`);
+  } catch {
+    return {};
+  }
+}
+
+export async function saveMovie(movie) {
+  if (!movie._id) {
+    return http.post(apiEndpoint, movie);
+  }
+
+  let { data: movieInDb } = await getMovie(movie._id);
+
+  const movieId = movieInDb._id;
+  movieInDb.title = movie.title;
+  movieInDb.genreId = movie.genreId;
+  movieInDb.numberInStock = movie.numberInStock;
+  movieInDb.dailyRentalRate = movie.dailyRentalRate;
+  delete movieInDb._id;
+  delete movieInDb.genre;
+
+  return http.put(`${apiEndpoint}/${movieId}`, movieInDb);
+}
